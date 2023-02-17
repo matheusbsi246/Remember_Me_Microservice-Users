c4079dc894ae6d91ea52db1963eb16ced8dfe91d
matheusbsi246 <matheusonofre@ufu.br>
2023-02-17 14:19:08 -0300

    hello world

---
+++ src/main/java/com/api/remembermeusers/Controller/UsersController.java
@@ -0,0 +1,16 @@
+package com.api.remembermeusers.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/v1/users")
+public class UsersController {
+
+    @GetMapping()
+    public String hello(){
+        return "Hello from users";
+    }
+}
